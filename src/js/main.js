0c425556e61d259fdd76280748a708e1e7007497
XV19444 <niklas.helling@ruv.de>
2025-08-17 16:03:58 +0200

    feat: initial fitness qiuz

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ import DarkMode from './darkMode.js';
 import { PlanGenerator } from './planGenerator.js';
 import { PDFExporter } from './pdfExporter.js';
 import { CalorieCalculator } from './calorieCalculator.js';
+import {FitnessQuiz} from "./quiz.js";
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const darkMode = new DarkMode('darkToggle');
     const planGenerator = new PlanGenerator('planForm', 'result', 'loader');
     const pdfExporter = new PDFExporter('exportPDF', planGenerator);
+    const quiz = new FitnessQuiz("quiz", "nextBtn", "result");
     const calorieCalculator = new CalorieCalculator('berechneKalorien', 'kalorienResult');
 });
